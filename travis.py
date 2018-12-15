6c5805954f4ed9b9d374e783244d1c239afb1422
douglas <ccydouglas327@hotmail.com>
2018-12-14 22:29:10 -0800

    Update travis.py
    final version of travis

--- travis.py
+++ travis.py
@@ -1,12 +1,25 @@
 known_users=["Alice","Bill","Coco","Dickson","Emily"]
 
-#print(len(known_users))
-
 while True:
     print("Hi! My name is Travis")
 
     name=input("What is your name?").strip().capitalize()
     if name in known_users:
         print("Hi {}. Welcome back!".format(name))
+        remove=input("Do you want to be removed from the system(y/n)").lower()
+
+        if remove=="y":
+            print("list before: {}".format(known_users))
+            known_users.remove(name)
+            print("list after: {}".format(known_users))
+        elif remove=="n":
+            print:("No problem.")
     else:
-        print("Name NOT recognised")
+        print("I don't think I have met you yet {}".format(name))
+        add_me=input("Would you like to be added to the system(y/n)").strip().lower()
+        if add_me== "y":
+            print("list before: {}".format(known_users))
+            known_users.append(name)
+            print("list after: {}".format(known_users)) 
+        elif add_me=="n":
+            print("No problem!")
