e75f7a15b1e61294446c8e271135311fcab483fd
douglas <ccydouglas327@hotmail.com>
2018-12-28 22:29:59 -0800

    Update tictactoe.py

--- tictactoe.py
+++ tictactoe.py
@@ -11,12 +11,58 @@ def print_board():
     print(row3)
     print()
 
+def player_move(icon):
 
+    if icon=="X":
+        number=1
+    elif icon=="O":
+        number=2
+
+    print("Your turn player{}".format(number))
+
+    goOn=True
+    
+    while goOn:
+        choice= int(input("Enter your move(1-9): ").strip())
+        if board[choice-1]=="   ":
+            board[choice-1]=icon
+            goOn=False
+        else:
+            print()
+            print("That space is taken.")
+
+def is_Victory(icon):
+    if(board[0]==icon and board[1]==icon and board[2]==icon)or\
+    (board[3]==icon and board[4]==icon and board[5]==icon)or\
+    (board[6]==icon and board[7]==icon and board[8]==icon)or\
+    (board[0]==icon and board[3]==icon and board[6]==icon)or\
+    (board[1]==icon and board[4]==icon and board[7]==icon)or\
+    (board[2]==icon and board[5]==icon and board[8]==icon)or\
+    (board[0]==icon and board[4]==icon and board[8]==icon)or\
+    (board[2]==icon and board[4]==icon and board[6]==icon):
+        return True
+    else:
+        return False
+
+def is_draw():
+    if "   " not in board:
+        return True
+    else:
+        return False
+    
 while True:
     print_board()
-    choice= int(input("Enter your move(1-9): ").strip())
-    if board[choice-1]=="   ":
-        board[choice-1]=" X "
-    else:
-        print()
-        print("That space is taken.")
+    player_move("X")
+    print_board()
+    if is_Victory("X"):
+        print("X wins!")
+        break
+    elif is_draw():
+        print("It is a draw!")
+        break
+    player_move("O")
+    if is_Victory("O"):
+        print("O wins!")
+        break
+print_board()
+        
