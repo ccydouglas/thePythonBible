4d4f88d3be978fb88930012d3bfafdb401336daf
douglas <ccydouglas327@hotmail.com>
2018-12-29 16:07:59 -0800

    Update Coin.py

--- Coin.py
+++ Coin.py
@@ -1,11 +1,29 @@
+import random
 class Pound:
-    
-    value=1.00
-    color="gold"
-    num_edges=1
-    diameter=22.5
-    thickness=3.15
-    heads=True
+    def __init__(self,rare=False):
+        self.rare=rare
+        if self.rare:
+            self.value=1.25
+        else:
+            self.value=1.00
+            
+        self.color="gold"
+        self.num_edges=1
+        self.diameter=22.5
+        self.thickness=3.15
+        self.heads=True
 
-coin1=Pound()
-print(coin1.value)
+    def __del__(self):
+        print("Coin Spend")
+
+    def rust(self):
+        self.color="greenish"
+
+    def clean(self):
+        self.color="gold"
+
+    def flip(self):
+        heads_options=[True,False]
+        choice=random.choice(heads_options)
+        self.heads=choice
+    
